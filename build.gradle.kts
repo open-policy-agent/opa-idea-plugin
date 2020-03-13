8f68baddd7b5360193cd264a23c4d3147ac85ab5
Vincent Gramer <vgramer@gmail.com>
2020-03-13 23:23:20 +0100

    migrate build.gradle to kotlin dsl
    migrate in order to user the grammarkit task to generate the lexer and parser
    
    Signed-off-by: Vincent Gramer <vgramer@gmail.com>

---
+++ build.gradle.kts
@@ -0,0 +1,100 @@
+import org.gradle.api.JavaVersion.VERSION_1_8
+import org.gradle.api.internal.HasConvention
+import org.jetbrains.intellij.tasks.RunIdeTask
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.grammarkit.tasks.GenerateLexer
+import org.jetbrains.grammarkit.tasks.GenerateParser
+
+plugins {
+    idea
+    kotlin("jvm") version "1.3.50"
+    id("org.jetbrains.intellij") version "0.4.16"
+    id("org.jetbrains.grammarkit") version "2020.1"
+    id("de.undercouch.download") version "3.4.3"
+    id("net.saliman.properties") version "1.4.6"
+
+}
+
+
+apply {
+    plugin("idea")
+    plugin("kotlin")
+    plugin("org.jetbrains.grammarkit")
+    plugin("org.jetbrains.intellij")
+
+}
+
+group = "com.github.vgramer"
+version = "1.0.0-SNAPSHOT"
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+idea {
+    module {
+        generatedSourceDirs.add(file("src/main/gen"))
+
+        // https://github.com/gradle/kotlin-dsl/issues/537/
+        excludeDirs = excludeDirs + file("testData") + file("deps")
+    }
+}
+
+intellij {
+    version = prop("ideaVersion")
+}
+
+sourceSets {
+    main {
+        java.srcDirs("src/main/gen")
+        kotlin.srcDirs("src/main/kotlin")
+        resources.srcDirs("src/main/resources")
+    }
+    test {
+        kotlin.srcDirs("src/test/kotlin")
+        resources.srcDirs("src/test/resources")
+    }
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = VERSION_1_8
+    targetCompatibility = VERSION_1_8
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            languageVersion = "1.3"
+            apiVersion = "1.3"
+            freeCompilerArgs = listOf("-Xjvm-default=enable")
+        }
+    }
+
+    withType<RunIdeTask> {
+        jvmArgs("--add-exports", "java.base/jdk.internal.vm=ALL-UNNAMED")
+    }
+}
+
+
+
+fun prop(name: String): String =
+    extra.properties[name] as? String
+        ?: error("Property `$name` is not defined in gradle.properties")
+
+val SourceSet.kotlin: SourceDirectorySet
+    get() =
+        (this as HasConvention)
+            .convention
+            .getPlugin(KotlinSourceSet::class.java)
+            .kotlin
+
+
+fun SourceSet.kotlin(action: SourceDirectorySet.() -> Unit) =
+    kotlin.action()
