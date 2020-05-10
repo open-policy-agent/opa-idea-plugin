ae5a2de10a1b138ac37f3b7e92702db0cf4b1513
Vincent Gramer <vgramer@gmail.com>
2020-05-10 22:11:20 +0200

    RegoTokenType: use defaut toString because it's produce better error message
    Signed-off-by: Vincent Gramer <vgramer@gmail.com>

--- src/main/kotlin/com/github/vgramer/opaplugin/lang/psi/RegoTokenType.kt
+++ src/main/kotlin/com/github/vgramer/opaplugin/lang/psi/RegoTokenType.kt
@@ -3,6 +3,4 @@ package com.github.vgramer.opaplugin.lang.psi
 import com.github.vgramer.opaplugin.lang.RegoLanguage
 import com.intellij.psi.tree.IElementType
 
-class RegoTokenType(debugName: String): IElementType(debugName, RegoLanguage) {
-    override fun toString(): String = "RegoTokenType." + super.toString()
-}
+class RegoTokenType(debugName: String) : IElementType(debugName, RegoLanguage)
