7944db4b31a5483140674f3693327f80b2d2ad5d
Andrea Canonica <andrea.canonica@inventage.com>
2023-03-31 10:08:33 +0200

    Compatibility 232 (#143)
    * Update Kotlin JVM Version
    * Update Intellij Plugin Version
    * Update Supported-Until Version
    * Updated Supported-Since Version
    
    Signed-off-by: Andrea Canonica <andrea.canonica@inventage.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ idea {
 plugins {
     idea
     kotlin("jvm") version "1.7.21"
-    id("org.jetbrains.intellij") version "1.13.2"
+    id("org.jetbrains.intellij") version "1.13.3"
     id("org.jetbrains.grammarkit") version "2021.2.2"
 }
 
